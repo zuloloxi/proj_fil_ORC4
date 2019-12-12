327dd057feaf7afa21b435cdefc5fed79d985b90
ovanprad <ovppub@free.fr>
2019-12-12 16:35:40 +0100

    Ajout 1 champ

--- src/main/java/com/dojo/stage/domain/Collaborator.java
+++ src/main/java/com/dojo/stage/domain/Collaborator.java
@@ -1,26 +1,25 @@
 package com.dojo.stage.domain;
 
 public class Collaborator {
-    String uid;
-    String Civilité;
-    String Nom;
-    String Prénom;
-    String Fonction;
-    String Téléphone;
-    String Mail;
-    String Etage;
-    String CodeImmeuble;
-//    Région
-//    Code région
-//    Niveau d'un territoirte
-//    Code territoire
-//    Niveau d'une entité
-//    Code entité
-//    Niveau d'une agence
-//    Code agence
-//    Localisation du collaborateur
-//            pj
-//    Collaborateurs à migrer CS D7
-//    Oui / Non
+    private String uid;
+    private String Civilite;
+    private String Nom;
+    private String Prenom;
+    private String Fonction;
+    private String Telephone;
+    private String Mail;
+    private String Etage;
+    private String CodeImmeuble;
+    private String Region;
+    private String regionCode;
+    private String territoryLevel;
+    private String territoiryCode;
+    private String entityLevel;
+    private String entityCode;
+    private String agencyLevel;
+    private String agencyCode;
+    private String localisation;
+    private String pj;
+    private boolean toMigrate;
 
 }
