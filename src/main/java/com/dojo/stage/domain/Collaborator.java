177ac5b987c471decf3011d0134feffd28e71423
ovanprad <ovppub@free.fr>
2019-12-12 16:27:16 +0100

    Initialisation

---
+++ src/main/java/com/dojo/stage/domain/Collaborator.java
@@ -0,0 +1,26 @@
+package com.dojo.stage.domain;
+
+public class Collaborator {
+    String uid;
+    String Civilité;
+    String Nom;
+    String Prénom;
+    String Fonction;
+    String Téléphone;
+    String Mail;
+    String Etage;
+//    Code immeuble de l'emplacement du collaborateur
+//    Région
+//    Code région
+//    Niveau d'un territoirte
+//    Code territoire
+//    Niveau d'une entité
+//    Code entité
+//    Niveau d'une agence
+//    Code agence
+//    Localisation du collaborateur
+//            pj
+//    Collaborateurs à migrer CS D7
+//    Oui / Non
+
+}
---
+++ src/test/java/com/dojo/stage/StageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.stage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
