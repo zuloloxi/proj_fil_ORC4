b809492216dfac193ccf39cb010d76d16a27a8ef
ovanprad <ovppub@free.fr>
2019-12-13 11:47:20 +0100

    création des classes d'infrastructure

---
+++ src/main/java/com/dojo/stage/infrastructure/OutputFileDAO.java
@@ -0,0 +1,17 @@
+package com.dojo.stage.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface OutputFileDAO  extends JpaRepository<OutputFileJPA, Long> {
+
+//    List<Library> findByType (Type type);
+//    List<Library> findByAddress_Number (int number);
+//    @Query (value = "SELECT * FROM LIBRARY WHERE Director_Surname = :name", nativeQuery = true)
+//    List<Library> toto (@Param("name") String name);
+//    @Query (value = "SELECT lib FROM LIBRARY lib WHERE lib.director.surname = ?1")
+////    @Query (value = "FROM LIBRARY WHERE director.surname = ?1")
+//    List<Library> findByDirectorSurname (String surName);
+//    @Query (value = "SELECT * FROM LIBRARY WHERE :where", nativeQuery = true)
+//    List<Library> findWhere (@Param("where") String where);
+}
