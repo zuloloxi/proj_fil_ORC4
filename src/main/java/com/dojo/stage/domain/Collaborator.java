e801be4284825336c746abdea3ca29bee35d424d
bougis <regis.bouchez@gmail.com>
2019-12-13 11:22:23 +0100

    ajout regles

--- src/main/java/com/dojo/stage/domain/Collaborator.java
+++ src/main/java/com/dojo/stage/domain/Collaborator.java
@@ -1,25 +1,139 @@
 package com.dojo.stage.domain;
 
-public class Collaborator {
-    private String uid;
-    private String civility;
-    private String name;
-    private String surname;
-    private String function;
-    private String phoneNumber;
-    private String eMail;
-    private String step;
-    private String builingCode;
-    private String state;
-    private String stateCode;
-    private String territoryLevel;
-    private String territoiryCode;
-    private String entityLevel;
-    private String entityCode;
-    private String agencyLevel;
-    private String agencyCode;
-    private String localisation;
-    private String pj;
-    private String toMigrate;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+
+public class Collaborator {
+
+    private String uid;
+    private String civilite;
+    private String nom;
+    private String prenom;
+    private String fonction;
+    private String telephone;
+    private String mail;
+    private String etage;
+    private String uoAffectation;
+    private String codeImmeubleEmplacementCollaborateur;
+    private String region;
+    private String codeRegion;
+    private String niveauTerritoirte;
+    private String codeTerritoire;
+    private String niveauEntite;
+    private String codeEntite;
+    private String niveauAgence;
+    private String codeAgence;
+    private String localisationCollaborateur;
+    private String pj;
+    private String aMigrer;
+
+    public void Collaborator(){}
+
+    public Collaborator(String uid, String civilite, String nom, String prenom, String fonction, String telephone, String mail, String etage, String uoAffectation, String codeImmeubleEmplacementCollaborateur, String region, String codeRegion, String niveauTerritoirte, String codeTerritoire, String niveauEntite, String codeEntite, String niveauAgence, String codeAgence, String localisationCollaborateur, String pj, String aMigrer) {
+        this.uid = uid;
+        this.civilite = civilite;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.fonction = fonction;
+        this.telephone = telephone;
+        this.mail = mail;
+        this.etage = etage;
+        this.uoAffectation = uoAffectation;
+        this.codeImmeubleEmplacementCollaborateur = codeImmeubleEmplacementCollaborateur;
+        this.region = region;
+        this.codeRegion = codeRegion;
+        this.niveauTerritoirte = niveauTerritoirte;
+        this.codeTerritoire = codeTerritoire;
+        this.niveauEntite = niveauEntite;
+        this.codeEntite = codeEntite;
+        this.niveauAgence = niveauAgence;
+        this.codeAgence = codeAgence;
+        this.localisationCollaborateur = localisationCollaborateur;
+        this.pj = pj;
+        this.aMigrer = aMigrer;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public String getCivilite() {
+        return civilite;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getFonction() {
+        return fonction;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getEtage() {
+        return etage;
+    }
+
+    public String getUoAffectation() {
+        return uoAffectation;
+    }
+
+    public String getCodeImmeubleEmplacementCollaborateur() {
+        return codeImmeubleEmplacementCollaborateur;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getCodeRegion() {
+        return codeRegion;
+    }
+
+    public String getNiveauTerritoirte() {
+        return niveauTerritoirte;
+    }
+
+    public String getCodeTerritoire() {
+        return codeTerritoire;
+    }
+
+    public String getNiveauEntite() {
+        return niveauEntite;
+    }
+
+    public String getCodeEntite() {
+        return codeEntite;
+    }
+
+    public String getNiveauAgence() {
+        return niveauAgence;
+    }
+
+    public String getCodeAgence() {
+        return codeAgence;
+    }
+
+    public String getLocalisationCollaborateur() {
+        return localisationCollaborateur;
+    }
+
+    public String getPj() {
+        return pj;
+    }
+
+    public String getaMigrer() {
+        return aMigrer;
+    }
 }
