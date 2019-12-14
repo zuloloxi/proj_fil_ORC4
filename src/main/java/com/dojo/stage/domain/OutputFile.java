8d345f86a1ba79abb6ae4e24acd7cd4b30a5ea4b
Guichonz <devdreamteam@free.fr>
2019-12-14 21:11:41 +0100

    Add default constructor to OutputFile

--- src/main/java/com/dojo/stage/domain/OutputFile.java
+++ src/main/java/com/dojo/stage/domain/OutputFile.java
@@ -108,6 +108,8 @@ public class OutputFile {
           this.commentaires = "";
      }
 
+     public OutputFile(){};
+
      public String getIdentifiant() {
           return identifiant;
      }
