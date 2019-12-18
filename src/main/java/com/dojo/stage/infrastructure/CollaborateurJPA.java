579c1bfc8c9944eba61b59dd312ac8d45464a8fb
KundaAstratta <krystof.humbert@gmail.com>
2019-12-18 20:17:28 +0100

    Added : (strategy= Generation.Type.IDENTITY) dans CollaborateurJPA (la génération de l'id se fera à partir du SGBD)

--- src/main/java/com/dojo/stage/infrastructure/CollaborateurJPA.java
+++ src/main/java/com/dojo/stage/infrastructure/CollaborateurJPA.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class CollaborateurJPA {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name="ID")
     private Long id;
 
