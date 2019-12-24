a5a2f28919abf3659dfa2279aefa4c7e64de9a03
ovanprad <ovppub@free.fr>
2019-12-24 10:44:29 +0100

    ajout du collaborateur.sql permettant de charger la table collaborateur (input). Renommage du data.sql en regle.sql

--- src/main/java/com/dojo/stage/StageApplication.java
+++ src/main/java/com/dojo/stage/StageApplication.java
@@ -2,13 +2,19 @@ package com.dojo.stage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class StageApplication {
+public class StageApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StageApplication.class, args);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
 
 }
